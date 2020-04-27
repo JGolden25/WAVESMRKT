f4bf25faf96fa5794d8b9c2843c0f53650cc2ca6
JGolden25 <joechre@gmail.com>
2020-04-27 16:37:23 -0500

    adds salt for hash password

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
     email:{
@@ -38,6 +40,22 @@ const userSchema = mongoose.Schema({
         type:String
     }
 });
+
+userSchema.pre('save',function(next){
+    var user = this;
+
+    bcrypt.genSalt(SALT_I,function(err,salt){
+        if(err) return next(err)
+
+        bcrypt.hash(user.password,salt,function(err,hash){
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        })
+    })
+})
+
+
 
 const User = mongoose.model('User', userSchema);
 
