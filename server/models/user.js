9968094188c6367a4f6aa84c5856683138d3b06e
JGolden25 <joechre@gmail.com>
2020-04-27 20:57:06 -0500

    authworkssuccessfullogin

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
     email:{
@@ -44,21 +46,39 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save',function(next){
     var user = this;
 
-    if(user.isModified()){
-    bcrypt.genSalt(SALT_I,function(err,salt){
-        if(err) return next(err)
-
-        bcrypt.hash(user.password,salt,function(err,hash){
+    if(user.isModified('password')){
+        bcrypt.genSalt(SALT_I,function(err,salt){
             if(err) return next(err);
-            user.password = hash;
-            next();
-        });
+    
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        })
+    } else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(candidatePassword,cb){
+    bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
+        if(err) return cb(err);
+        cb(null,isMatch)
     })
 }
-else{
-    next()
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),process.env.SECRET)
+
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
 }
-})
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -26,14 +26,28 @@ app.post('/api/users/register',(req,res)=>{
        if(err) return res.join({success:false,err});
        res.status(200).json({
            success: true,
-           userdata: doc
        })
     })
-})
+});
 
 app.post('/api/users/login',(req,res)=>{
     //find email
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user) return res.json({loginSuccess:false,message:'Auth failed, email not found'});
+        //check pass
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'Wrong password'});
+            //gnerate token
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth',user.token).status(200).json({
+                    loginSuccess: true
+                })
+            })
+        })
+    })
 })
+
 
 const port = process.env.PORT || 3002;
 app.listen(port,()=>{
