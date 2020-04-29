90ad8fe4fa229e549290753a757306e55236706e
JGolden25 <joechre@gmail.com>
2020-04-29 13:15:21 -0500

    woods and products included

--- server/server.js
+++ server/server.js
@@ -16,11 +16,35 @@ app.use(cookieParser());
 //MODELS//
 
 const { User } = require('./models/user');
-const { Brand } = require('./models/brand')
+const { Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
+
 
 //Middlewares
 const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
+
+//Woods//
+
+app.post('/api/product/wood',auth,admin,(req,res)=>{
+    const wood = new Wood(req.body);
+
+    wood.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success:true,
+            brand: doc
+
+        })
+    })
+});
+
+app.get('/api/product/woods',(req,res)=>{
+    Wood.find({},(err,woods)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).send(woods)
+    })
+})
 
 //Brand//
 
