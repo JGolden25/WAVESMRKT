0fc783b2f2dea0af016cf25b312378d7dea51eda
JGolden25 <joechre@gmail.com>
2020-05-01 14:28:49 -0500

    home page renders and server running concurrently

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './Resources/css/styles.css';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
