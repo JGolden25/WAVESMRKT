5e2bb650d5833f278f4d9d07dc27e08c586cfed0
JGolden25 <joechre@gmail.com>
2020-05-07 23:47:52 -0500

    redux setup complete and servers back running

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,17 @@ import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 
+import Reducer from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
+
 
 ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(Reducer , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
   <BrowserRouter>
     <Routes />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
