ad23d937358a08088eaf59ef72326244a3d27e24
JGolden25 <joechre@gmail.com>
2020-05-11 23:17:36 -0500

    register route and client page started

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Layout from './hoc/layout';
 import Home from './Components/Home';
 import RegisterLogin from './Components/Register_login';
+import Register from './Components/Register_login/register'
 
 const Routes = () => {
   return(
     <Layout>
 <Switch>
+<Route path="/register" exact component={Register}/>
 <Route path="/register_login" exact component={RegisterLogin}/>
     <Route path="/" exact component={Home}/>
 </Switch>
