ff54f4741c5ae462b6160969e588a197c4222874
JGolden25 <joechre@gmail.com>
2020-05-13 18:19:31 -0500

    setting up cards images for home page

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import HomeSlider from './home_slider';
+import HomePromotion from './home_promotion';
+import CardBlock from '../utils/card_block';
+
+import { connect } from 'react-redux';
+import { getProductsBySell, getProductsByArrival } from '../../actions/products_actions';
 
 class Home extends Component {
+
+
+    componentDidMount(){
+        this.props.dispatch(getProductsBySell());
+        this.props.dispatch(getProductsByArrival());
+    }
+
     render() {
         return(
             <div>
-                home
+                <HomeSlider/>
+                <CardBlock
+                list={this.props.products.bySell}
+                title="Best Selling Guiatrs"
+                />
+                <HomePromotion/>
+                <CardBlock
+                list={this.props.products.byArrival}
+                title="New Arrivals"
+                />
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps)(Home);
---
+++ client/src/Components/utils/Form/card.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class Card extends Component {
+    
+    renderCardImage(images){
+        if(images.length > 0){
+            return images[0].url
+        }else{
+            return '/images/image_not_available.png'
+        }
+    }
+
+
+    render(){
+        const props = this.props;
+        return (
+        <div className={`card_item_wrapper ${props.grid}`}>
+            <div
+            className="image"
+            style={{
+                background:`url(${this.renderCardImage(props.images)}) no-repeat`
+            }}
+            >
+                <div className="action_container">
+                        <div className="tags">
+                            <div className="brand">{props.brand.name}</div>
+                            <div className="name">{props.name}</div>
+                            <div className="name">${props.price}</div>
+                        </div>
+                        </div>
+                        { props.grid ?
+                        <div className="description">
+                                    kshdjshdsdjshd
+                        </div>
+                        :null
+                    }
+            </div>
+        </div>
+    );
+}
+}
+
+export default Card;
