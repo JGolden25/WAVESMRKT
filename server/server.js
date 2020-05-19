3d12c13fa8673e646233da18e624e83d518404c1
JGolden25 <joechre@gmail.com>
2020-05-18 21:56:38 -0500

    Completes add to cart functionality for both buttons

--- server/server.js
+++ server/server.js
@@ -255,37 +255,45 @@ app.get('/api/users/removeimage',auth,admin,(req,res)=>{
     })
 })
 
-app.post('/api/users/addToCart', auth,(req,res)=>{
+app.post('/api/users/addToCart',auth,(req,res)=>{
 
     User.findOne({_id: req.user._id},(err,doc)=>{
         let duplicate = false;
 
         doc.cart.forEach((item)=>{
-            if(item.id === req.query.productId){
-                duplicate = true;
+            if(item.id == req.query.productId){
+                  duplicate = true;  
             }
         })
 
         if(duplicate){
-
+            User.findOneAndUpdate(
+                {_id: req.user._id, "cart.id":mongoose.Types.ObjectId(req.query.productId)},
+                { $inc: { "cart.$.quantity":1 } },
+                { new:true },
+                ()=>{
+                    if(err) return res.json({success:false,err});
+                    res.status(200).json(doc.cart)
+                }
+            )
         } else {
             User.findOneAndUpdate(
                 {_id: req.user._id},
                 { $push:{ cart:{
-                    id:mongoose.Types.ObjectId(req.query.productId), 
+                    id: mongoose.Types.ObjectId(req.query.productId),
                     quantity:1,
-                    date:Date.now()
+                    date: Date.now()
                 } }},
                 { new: true },
                 (err,doc)=>{
-                    if(err) return res.json({success: false, err});
+                    if(err) return res.json({success:false,err});
                     res.status(200).json(doc.cart)
                 }
             )
         }
     })
-
 })
+
 
 
 const port = process.env.PORT || 3002;
