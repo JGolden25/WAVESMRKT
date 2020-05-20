d98a6669b221d14d01f939d737a2b0d44924e889
JGolden25 <joechre@gmail.com>
2020-05-20 13:15:42 -0500

    fixes site_reducer and adds site to index

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -21,3 +21,6 @@ export const ADD_PRODUCT = 'add_product';
 export const CLEAR_PRODUCT = 'clear_product'
 export const GET_PRODUCT_DETAIL = 'get_product_detail';
 export const CLEAR_PRODUCT_DETAIL = 'clear_product_detail';
+
+export const GET_SITE_DATA = 'get_site_data';
+export const UPDATE_SITE_DATA = 'update_site_data';
--- client/src/routes.js
+++ client/src/routes.js
@@ -13,6 +13,7 @@ import AddProduct from './Components/User/Admin/add_product';
 import ManageCategories from './Components/User/Admin/manage_categories'
 import UserCart from './Components/User/cart'
 import UpdateProfile from './Components/User/update_profile';
+import ManageSite from './Components/User/Admin/manage_site';
 
 const Routes = () => {
   return(
@@ -23,6 +24,7 @@ const Routes = () => {
 <Route path="/user/user_profile" exact component={Auth(UpdateProfile,true)}/>
 <Route path="/admin/add_product" exact component={Auth(AddProduct,true)}/>
 <Route path="/admin/manage_categories" exact component={Auth(ManageCategories,true)}/>
+<Route path="/admin/site_info" exact component={Auth(ManageSite,true)}/>
 
 <Route path="/product_detail/:id" exact component={Auth(ProductPage,null)}/>
 <Route path="/register" exact component={Auth(Register,false)}/>
