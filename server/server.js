19ad034c1aea190d58a81f71338278cc0b22d080
JGolden25 <joechre@gmail.com>
2020-05-26 18:48:15 -0500

    email sent with nodemailer

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const formidable = require('express-formidable');
 const cloudinary = require('cloudinary');
-// const mailer = require('nodemailer');
+const mailer = require('nodemailer');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -38,30 +38,30 @@ const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
 
 
-// const smtpTransport = mailer.createTransport({
-//     service:"Gmail",
-//     auth:{
-//         user:"waves.guitars.rev@gmail.com",
-//         pass:"fulltone1985"
-//     }
-// });
+const smtpTransport = mailer.createTransport({
+    service:"Gmail",
+    auth:{
+        user:"wavesguitarsmrkt@gmail.com",
+        pass:"wavesguitars1"
+    }
+});
 
-// var mail = {
-//     from:"Waves <waves.guitars.rev@gmail.com",
-//     to:"joechre@gmail.com",
-//     subject:"Send test email",
-//     text:"Testing out waves mails",
-//     html:"<b>Hello guys this works</b>"
-// }
+var mail = {
+    from:"Waves <waves.guitars.rev@gmail.com",
+    to:"joechre@gmail.com",
+    subject:"Send test email",
+    text:"Testing out waves mails",
+    html:"<b>Hello guys this works</b>"
+}
 
-// smtpTransport.sendMail(mail,function(error,response){
-//     if(error){
-//         console.log(error);
-//     } else {
-//         console.log('email sent')
-//     }
-//     smtpTransport.close();
-// })
+smtpTransport.sendMail(mail,function(error,response){
+    if(error){
+        console.log(error);
+    } else {
+        console.log('email sent')
+    }
+    smtpTransport.close();
+})
 
 //Products//
 
