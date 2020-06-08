9a636a55f282eb460f9de3604caa1b872c321819
JGolden25 <joechre@gmail.com>
2020-06-08 09:50:08 -0500

    another edit to server before deploy

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ require('dotenv').config();
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE)
 
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
