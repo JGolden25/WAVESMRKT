c7b047c706588ce77f97c7c93cf8ffcd7e0d85cc
JGolden25 <joechre@gmail.com>
2020-06-11 09:26:25 -0500

    mongodburi added to env

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const async = require('async');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.MONGODB_URI)
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
